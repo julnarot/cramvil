7ad639bd6545cafa403adc31f0b5ccb5ea6b7595
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-24 23:58:59 -0500

    feat(tasks): it was added dashboard and login module to router

--- tasks/src/app/app-routing.module.ts
+++ tasks/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((i) => i.AuthModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((d) => d.DashboardModule),
+      canActivate: [AuthGuardService]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
