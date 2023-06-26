28d68db1b3e132328c4557fa1ce30ad604a956fc
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-25 23:49:35 -0500

    conf(tasks_resource): task_resource application was initilized

---
+++ tasks_resource/src/main/java/com/julnarot/tasks_resource/TasksResourceApplication.java
@@ -0,0 +1,13 @@
+package com.julnarot.tasks_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksResourceApplication.class, args);
+	}
+
+}
---
+++ tasks_resource/src/test/java/com/julnarot/tasks_resource/TasksResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julnarot.tasks_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
