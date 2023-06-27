3f9faeb08776f81e9bef86ac14ff459081deced9
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-26 23:37:33 -0500

    feat(task): board container wad added

---
+++ tasks/src/app/features/board/components/board-container/board-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-container',
+  templateUrl: './board-container.component.html',
+  styleUrls: ['./board-container.component.scss']
+})
+export class BoardContainerComponent {
+
+}
