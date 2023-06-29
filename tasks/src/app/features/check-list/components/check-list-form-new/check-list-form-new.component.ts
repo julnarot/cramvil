a156b3b02e3e25302fa94c56d144fab9c44c4e6e
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-28 23:03:29 -0500

    feat(tasks): validator and badge for delimiter character
    Name task now complem limit text validator

--- tasks/src/app/features/check-list/components/check-list-form-new/check-list-form-new.component.ts
+++ tasks/src/app/features/check-list/components/check-list-form-new/check-list-form-new.component.ts
@@ -5,14 +5,30 @@ import { TaskFacade } from 'src/app/core/facades/task-facade';
 @Component({
   selector: 'app-check-list-form-new',
   templateUrl: './check-list-form-new.component.html',
-  styleUrls: ['./check-list-form-new.component.scss']
+  styleUrls: ['./check-list-form-new.component.scss'],
 })
 export class CheckListFormNewComponent {
-  taskNameFmCtrl = new FormControl('', [Validators.required]);
-  constructor(private readonly taskFacades: TaskFacade) {}
+  maxLen = 70;
+  minLen = 4;
+  charsAvailable = 0;
+  taskNameFmCtrl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(this.minLen),
+    Validators.maxLength(this.maxLen),
+  ]);
+  constructor(private readonly taskFacades: TaskFacade) {
+    this.taskNameFmCtrl.valueChanges.subscribe((s) => {
+      if (s && typeof s === 'string') {
+        this.charsAvailable = s.length;
+      }
+    });
+  }
   onSaveNewTask() {
     if (this.taskNameFmCtrl.valid && this.taskNameFmCtrl.value) {
       this.taskFacades.saveNewTask(this.taskNameFmCtrl.value).subscribe();
     }
+  }
+  onCancelAdding() {
+    this.taskFacades.cancelAddingNewTask();
   }
 }
