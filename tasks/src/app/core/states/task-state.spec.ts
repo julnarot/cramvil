e74eaf4cbe7371a36d796a67f3cd2bcdad9382b6
@Julnarot <rauljhonatan.tola@gmail.com>
2023-06-30 23:56:19 -0500

    test(task/task-state): Unit testing for task state

--- tasks/src/app/core/states/task-state.spec.ts
+++ tasks/src/app/core/states/task-state.spec.ts
@@ -1,7 +1,34 @@
+import { TestBed } from '@angular/core/testing';
 import { TaskState } from './task-state';
+import { firstTaskResponseSpec } from '../spec-helpers/task-spec-helper';
+import { TaskAdapter } from '../adapters/task-adapter';
 
 describe('TaskState', () => {
-  it('should create an instance', () => {
-    expect(new TaskState()).toBeTruthy();
+  let state: TaskState;
+  let adapter: TaskAdapter;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    state = TestBed.inject(TaskState);
+    adapter = TestBed.inject(TaskAdapter);
+  });
+  it('should be created', () => {
+    expect(state).toBeTruthy();
+  });
+
+  it('should be set and get task', () => {
+    const expectTask = adapter.adapt(firstTaskResponseSpec);
+    state.setTask(expectTask);
+    state.getTask$().subscribe((task) => {
+      expect(task).toEqual(expectTask);
+    });
+  });
+
+  it('should be set and get list task', () => {
+    const expectTasks = adapter.adaptArray([firstTaskResponseSpec]);
+    state.setTasks(expectTasks);
+    state.getTasks$().subscribe((tasks) => {
+      expect(tasks.length).toEqual(expectTasks.length);
+      expect(tasks[0]).toEqual(expectTasks[0]);
+    });
   });
 });
