a7522f3c57f97ca210d4317a209545c4e66cb2cb
@Julnarot <rauljhonatan.tola@gmail.com>
2023-07-08 23:50:06 -0500

    test(task/task-facade): Adding unit test for saved new task

--- tasks/src/app/core/facades/task-facade.spec.ts
+++ tasks/src/app/core/facades/task-facade.spec.ts
@@ -2,16 +2,26 @@ import { TestBed } from '@angular/core/testing';
 import { TaskFacade } from './task-facade';
 import { NbAuthModule } from '@nebular/auth';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TaskAdapter } from '../adapters/task-adapter';
+import { firstTaskResponseSpec } from '../spec-helpers/task-spec-helper';
 
 describe('TaskFacade', () => {
   let facade: TaskFacade;
+  let adapter: TaskAdapter;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NbAuthModule.forRoot(), HttpClientTestingModule],
     });
     facade = TestBed.inject(TaskFacade);
+    adapter = TestBed.inject(TaskAdapter);
   });
   it('should be created', () => {
     expect(facade).toBeTruthy();
+  });
+  it('should be saved task', () => {
+    let _task = adapter.adapt(firstTaskResponseSpec);
+    facade.saveNewTask(_task.name).subscribe((resultTask) => {
+      expect(resultTask).toEqual(_task);
+    });
   });
 });
--- tasks/src/app/core/facades/task-facade.ts
+++ tasks/src/app/core/facades/task-facade.ts
@@ -1,4 +1,4 @@
-import { Observable, Subject, take, tap } from 'rxjs';
+import { Observable, take, tap } from 'rxjs';
 import { Task } from '../models/task';
 import { TaskService } from '../services/task.service';
 import { TaskState } from '../states/task-state';
@@ -6,7 +6,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class TaskFacade {
-  _isAddingNewTask$ = new Subject<boolean>();
   tasks$ = this.taskState.getTasks$();
   constructor(
     private readonly taskService: TaskService,
@@ -15,26 +14,15 @@ export class TaskFacade {
   saveNewTask(newTaskName: string): Observable<Task> {
     const newTask = new Task();
     newTask.name = newTaskName;
-    this.addingNewTask();
     return this.taskService
       .saveNewTaskApi(newTask)
       .pipe(tap((taskSaved) => this.addNewTaskAction(taskSaved)));
   }
 
-  isAddingNewTask$(): Observable<boolean> {
-    return this._isAddingNewTask$.asObservable();
-  }
-  addingNewTask(): void {
-    this._isAddingNewTask$.next(true);
-  }
-  cancelAddingNewTask() {
-    this._isAddingNewTask$.next(false);
-  }
   addNewTaskAction(taskSaved: Task) {
     this.tasks$
       .pipe(
         take(1),
-        tap((_) => this.cancelAddingNewTask())
       )
       .subscribe((currentTaskList) =>
         this.addTaskToList(taskSaved, currentTaskList)
