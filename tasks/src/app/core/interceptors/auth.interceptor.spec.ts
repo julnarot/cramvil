bf4ff7c7ad540ae4014fd947cd7f752c33bdf60b
@Julnarot <rauljhonatan.tola@gmail.com>
2023-07-10 21:41:21 -0500

    fix(task/auth-interceptor): Adding authenticated condition for call token value

--- tasks/src/app/core/interceptors/auth.interceptor.spec.ts
+++ tasks/src/app/core/interceptors/auth.interceptor.spec.ts
@@ -2,17 +2,52 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
 import { NbAuthModule } from '@nebular/auth';
+import { HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable, Subscriber } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [NbAuthModule.forRoot()],
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  let authInterceptor: AuthInterceptor;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [NbAuthModule.forRoot()],
+      providers: [AuthInterceptor],
+    });
+    authInterceptor = TestBed.inject(AuthInterceptor);
+  });
 
   it('should be created', () => {
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
     expect(interceptor).toBeTruthy();
+  });
+
+  xit('should be set token', () => {
+    const specToken = 'aaabbbcccdddeee';
+    //   token: 'fake-token'
+
+    const next: HttpHandler = {
+      handle: () => {
+        return new Observable((subscriber: Subscriber<any>) => {
+          subscriber.complete();
+        });
+      },
+    };
+    const requestMock = new HttpRequest(
+      'GET',
+      environment.taskResource + '/task'
+    );
+
+    spyOn(next, 'handle').and.callThrough();
+
+    authInterceptor.intercept(requestMock, next).subscribe();
+
+    expect(next.handle).toHaveBeenCalledWith(
+      requestMock.clone({
+        headers: requestMock.headers.append(
+          'Authorization',
+          `Bearer ${specToken}`
+        ),
+      })
+    );
   });
 });
--- tasks/src/app/core/interceptors/auth.interceptor.ts
+++ tasks/src/app/core/interceptors/auth.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable, map, switchMap, tap } from 'rxjs';
+import { Observable, iif, map, switchMap } from 'rxjs';
 import { NbAuthService } from '@nebular/auth';
 
 @Injectable()
@@ -16,17 +16,24 @@ export class AuthInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    return this.authService.getToken().pipe(
-      map((token: any) => token.token['access_token']),
-      switchMap((token) => {
-        const newRequest = request.clone({
-          setHeaders: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        return next.handle(newRequest);
-      })
+    return this.authService.isAuthenticated().pipe(
+      switchMap((authenticated) =>
+        iif(
+          () => authenticated,
+          this.authService.getToken().pipe(
+            map((token: any) => token.token['access_token']),
+            switchMap((t) => {
+              const newRequest = request.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${t}`,
+                },
+              });
+              return next.handle(newRequest);
+            })
+          ),
+          next.handle(request)
+        )
+      )
     );
   }
 }
